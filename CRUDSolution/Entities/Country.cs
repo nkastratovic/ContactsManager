e3cde0951a41fbd29093a778385818e3d757fd1f
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-11-28 10:12:22 +0100

    Add Country Test.

---
+++ CRUDSolution/Entities/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities
+{
+    public class Country
+    {
+        /// <summary>
+        /// Domain Model for Country
+        /// </summary>
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+    }
+}
