8fb58227bced1e60eefc93b0614e00d4259fcce9
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-11-28 22:11:26 +0100

    Add Person Validation.

--- CRUDSolution/ServiceContracts/DTO/PersonAddRequest.cs
+++ CRUDSolution/ServiceContracts/DTO/PersonAddRequest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using ServiceContracts.Enums;
 using Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServiceContracts.DTO
 {
@@ -9,8 +10,13 @@ namespace ServiceContracts.DTO
   /// </summary>
   public class PersonAddRequest
   {
+    [Required(ErrorMessage = "Person Name can't be blank")]
     public string? PersonName { get; set; }
+
+    [Required(ErrorMessage = "Email can't be blank")]
+    [EmailAddress(ErrorMessage = "Email value should be a valid email")]
     public string? Email { get; set; }
+
     public DateTime? DateOfBirth { get; set; }
     public GenderOptions? Gender { get; set; }
     public Guid? CountryID { get; set; }
--- CRUDSolution/Services/PersonsService.cs
+++ CRUDSolution/Services/PersonsService.cs
@@ -2,6 +2,8 @@
 using Entities;
 using ServiceContracts.DTO;
 using ServiceContracts;
+using System.ComponentModel.DataAnnotations;
+using Services.Helpers;
 
 namespace Services
 {
@@ -34,11 +36,8 @@ namespace Services
         throw new ArgumentNullException(nameof(personAddRequest));
       }
 
-      //Validate PersonName
-      if (string.IsNullOrEmpty(personAddRequest.PersonName))
-      {
-        throw new ArgumentException("PersonName can't be blank");
-      }
+      //Model validation
+      ValidationHelper.ModelValidation(personAddRequest);
 
       //convert personAddRequest into Person type
       Person person = personAddRequest.ToPerson();
