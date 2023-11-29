36d4c9f3baa9288bd6471863c2ac54ea1bdbfdf9
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-11-29 11:33:16 +0100

    Add xUnit tests for GetFilteredPersons method.

--- CRUDSolution/Services/PersonsService.cs
+++ CRUDSolution/Services/PersonsService.cs
@@ -70,5 +70,10 @@ namespace Services
 
       return person.ToPersonResponse();
     }
+
+    public List<PersonResponse> GetFilteredPersons(string searchBy, string? searchString)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
