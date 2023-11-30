0cbd2ed9b93cc6def13f0d5e4ae7705fa405e0f6
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-11-30 10:00:10 +0100

    Add Mock data.

--- CRUDSolution/CRUDTests/PersonsServiceTest.cs
+++ CRUDSolution/CRUDTests/PersonsServiceTest.cs
@@ -22,7 +22,7 @@ namespace CRUDTests
     public PersonsServiceTest(ITestOutputHelper testOutputHelper)
     {
       _personService = new PersonsService();
-      _countriesService = new CountriesService();
+      _countriesService = new CountriesService(false);
       _testOutputHelper = testOutputHelper;
     }
 
