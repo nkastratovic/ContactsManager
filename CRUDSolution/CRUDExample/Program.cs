288722770feda367378057e72f380b6659228e0c
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-11-30 10:01:27 +0100

    List View.

--- CRUDSolution/CRUDExample/Program.cs
+++ CRUDSolution/CRUDExample/Program.cs
@@ -1,5 +1,13 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+//add services into IoC container
+builder.Services.AddSingleton<ICountriesService, CountriesService>();
+builder.Services.AddSingleton<IPersonsService, PersonsService>();
+
 
 var app = builder.Build();
 
